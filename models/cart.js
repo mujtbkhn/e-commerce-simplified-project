3654409bcd791ac0ec3a373773f1987abe00c61b
abdul rehman mujtaba <mujjukhn538@gmail.com>
2024-03-08 20:24:00 +0530

    added cart

---
+++ models/cart.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/codepth_assignment')
+
+const CartItemSchema = new mongoose.Schema({
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 1
+    }
+})
+
+const CartSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    items: [CartItemSchema]
+})
+
+const Cart = mongoose.model('Cart', CartSchema)
+
+module.exports = Cart
--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,73 @@
 const express = require('express')
-
+const Cart = require('../models/cart')
+const { authMiddleware } = require('../middlewares/authMiddleware')
 const router = express.Router()
+
+router.post('/add', authMiddleware, async (req, res) => {
+    try {
+        const userId = req.userId
+        const productId = req.body.productId
+
+        let cart = await Cart.findOne({ user: userId })
+        if (!cart) {
+            cart = new Cart({ user: userId, items: [] })
+        }
+
+        const existingItems = cart.items.find(item => item.product.toString() === productId)
+
+        if (existingItems) {
+            existingItems.quantity++
+        } else {
+            cart.items.push({ product: productId, quantity: 1 })
+        }
+
+        await cart.save()
+        res.json({
+            message: "product added successfully"
+            , cart
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+})
+
+router.get('/cart', authMiddleware, async (req, res) => {
+    try {
+        const userId = req.userId
+        const cart = await Cart.find({ user: userId })
+        if (!cart) {
+            return res.status(404).json({ message: "no cart found for this user" })
+        }
+        res.json({
+            cart
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+})
+router.delete('/cart/:id', authMiddleware, async (req, res) => {
+    try {
+        const userId = req.userId
+        const productId = req.body.productId
+
+        let cart = await Cart.findOne({ user: userId })
+        if (!cart) {
+            cart = new Cart({ user: userId, items: [] })
+        }
+
+       
+        const updatedItems = cart.items.filter(item => item.product.toString() !== productId);
+        cart.items = updatedItems;
+        await cart.save()
+        res.json({
+            message: "product deleted successfully"
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+})
 
 module.exports = router
